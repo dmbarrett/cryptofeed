092048da51fd7a95c1214f204b6f38891b4ccc67
joeypel <joeypelazza@gmail.com>
2020-01-23 14:07:46 -0500

    first commit with cache

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv").config({ path: ".env" });
 
-require("dotenv").config();
-
-const fetch = require("node-fetch");
+const getCache = require("./services/cache").getCache;
 
 const app = express();
-
-app.set("view engine", "pug");
-app.set("views", "views");
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -17,16 +13,6 @@ const url =
 const headers = {
   "X-CMC_PRO_API_KEY": process.env.APIKEY
 };
-app.get("/", (req, res) => {
-  fetch(url, { method: "GET", headers: headers })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  res.render("home");
-});
+app.get("/latestlistings", (req, res) => {res.json(getCache('latestListings'))});
 
 app.listen(9000);
